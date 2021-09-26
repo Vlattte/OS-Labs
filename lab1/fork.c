c3fcec7b3c9c90e62c7e3f199b9570350773c1fa
Vlattte <vladislav_smirnov01@mail.ru>
2021-09-26 14:32:03 +0300

    added child's exit status, pid&ppid in atexit

--- lab1/fork.c
+++ lab1/fork.c
@@ -5,7 +5,11 @@
 
 void at_exit()
 {
-	printf("HI, i'm Atexit called function, i'm done\n");
+	printf ("=============================================\n");
+	printf ("  HI, i'm Atexit called function, i'm done.\n");
+	printf ("  Я потомок процесса: %d\n", getppid());
+        printf ("  Мой PID: %d\n", getpid());
+        printf ("=============================================\n\n");
 }
 
 int main()
@@ -27,7 +31,9 @@ int main()
 			printf("-------\nPARENT:\n-------\n");
 			printf("Мой PID: %d\n", getpid());
 			printf("Мой родительский процесс: %d\n", getppid());
-			printf("Ожидание завершения потомка.\n");
-			wait(&status);
+			printf("Ожидание завершения потомка\n");
+			wait(&status); 
+			printf("Статус завершения потомка: %d\n", WEXITSTATUS(status));
 	}
+	return 0;
 }
