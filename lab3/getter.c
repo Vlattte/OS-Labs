c140219cf4508eeb16b501aeacb746eb658dda29
Vlattte <vladislav_smirnov01@mail.ru>
2021-11-22 12:41:52 +0300

    added lab3

---
+++ lab3/getter.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#define FILENAME "memory_buffer"
+
+typedef struct data
+{
+	pid_t fppid;	//first programm pid
+	time_t fptime;	//first programm time
+} data;
+
+int main(int agrc, char* argv[])
+{
+	key_t key = ftok(FILENAME, 8);
+	
+	if (key == -1)
+	{
+		printf("FTOK ERROR %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	int shmemid = shmget(key, sizeof(data), 0666);
+	if (shmemid < 0)
+	{
+		printf("SHMGET ERROR %s\n", strerror(errno));
+		exit(-2);
+	}
+
+	void* shmatptr = shmat(shmemid, NULL, 0);
+	if (shmatptr < 0)
+	{
+		printf("SHMAT ERROR %s\n", strerror(errno));
+		exit(-3);
+	}
+
+	data getted_memory = *((data*)shmatptr);
+	printf("\ngetted from shared memory: GIVER PID: %d\nGIVER TIME: %s", 
+		getted_memory.fppid, ctime(&getted_memory.fptime));
+	time_t cur_time = time(NULL);
+	printf("GETTER TIME: %s\n", ctime(&cur_time));
+
+	/*shmdt(shmatptr);
+	shmctl(shmemid, IPC_RMID, NULL);*/
+
+	return 0;
+}
---
+++ lab3/giver.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#define FILENAME "memory_buffer"
+
+typedef struct data
+{
+	pid_t fppid;	//first programm pid
+	time_t fptime;	//first programm time
+} data;
+
+
+int main(int agrc, char* argv[])
+{
+	struct shmid_ds shmid_ds;
+
+	key_t key = ftok(FILENAME, 8);
+	if (key == -1)
+	{
+		printf("FTOK ERROR %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	int shmemid = shmget(key, 8, IPC_CREAT | 0666);
+	if (shmemid < 0)
+	{
+		printf("SHMGET ERROR %s\n", strerror(errno));
+		exit(-2);
+	}
+
+	shmctl(shmemid, IPC_STAT, &shmid_ds);
+	
+	if (shmid_ds.shm_nattch >= 1)
+	{
+		printf("ERROR!!!! PROCCES ALREADY EXISTS!!!!\n");
+		exit(1);
+	}
+
+
+	while (1 == 1)
+	{
+		void* shmatptr = shmat(shmemid, NULL, 0);
+		
+		if (shmatptr < 0)
+		{
+			printf("SHMAT ERROR %s\n", strerror(errno));
+			exit(-3);
+		}
+
+		time_t cur_time = time(NULL);
+		data shared_memory = { getpid(),  cur_time };
+
+		*((data*)shmatptr) = shared_memory;
+		printf("\nplaced in shared memory: MY PID: %d\nMY TIME: %s\n",
+			shared_memory.fppid, ctime(&shared_memory.fptime));
+		sleep(5);
+
+		shmdt(shmatptr);	
+	}
+	return 0;
+}
