a9ab59b980b3141fe60bcdfa0a3ff68b6cd50f6d
Vlattte <vladislav_smirnov01@mail.ru>
2021-12-06 16:13:57 +0300

    added lab6

---
+++ lab6/cond.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+
+int arr = 0;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* write_fun(void* str)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&mutex);
+		arr++;
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_broadcast(&cond);
+		sleep(2);
+	}
+}
+
+void* read_fun(void* str)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&mutex);
+		pthread_cond_wait(&cond, &mutex);
+		printf("Cur val = %d\n", arr);
+		printf("Pthread ID - %lu\n", pthread_self());
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_t writer;
+	pthread_create(&writer, NULL, write_fun, NULL);
+
+	pthread_t reader[10];
+	for (int i = 0; i < 10; i++)
+		pthread_create(&reader[i], NULL, read_fun, NULL);
+	pthread_cond_broadcast(&cond);
+	pthread_join(writer, NULL);
+	return 0;
+}
+
+
